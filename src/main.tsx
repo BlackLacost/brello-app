08d85e50f89eccef660f175c12ddad5e52577141
Ilya Lisin <blacklacost@gmail.com>
2024-09-09 11:15:20 +0300

    init hasky

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import '@mantine/core'
-import { MantineProvider } from '@mantine/core'
-import '@mantine/hooks'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "@mantine/core";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/hooks";
+
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <MantineProvider>
     <App />
   </MantineProvider>,
-)
+);
